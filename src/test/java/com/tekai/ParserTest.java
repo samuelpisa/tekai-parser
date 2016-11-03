9310969e1947dbbc71d3e8dc2ffde59c572b471d
Samuel <samuel.pisa@tegra.me>
2016-11-03 14:51:07 -0200

    refactoring unit tests

--- src/test/java/com/tekai/ParserTest.java
+++ src/test/java/com/tekai/ParserTest.java
@@ -42,51 +42,51 @@ public class ParserTest {
         assertParsing("([abc]:FUNCTION ([+]:ARITHMETIC ([(]:PARENTHESIS ([+]:ARITHMETIC [2]:NUMBER [1]:NUMBER)) [3]:NUMBER))", "abc((2 + 1) + 3)");
         assertParsing("([+]:ARITHMETIC ([(]:PARENTHESIS ([+]:ARITHMETIC ([+]:ARITHMETIC [1]:NUMBER ([abc]:FUNCTION ([+]:ARITHMETIC [2]:NUMBER [3]:NUMBER) [4]:NUMBER)) [5]:NUMBER)) [6]:NUMBER)", "(1 + abc(2 + 3, 4) + 5) + 6");
         assertParsing("([abc]:FUNCTION ([def]:FUNCTION [1]:NUMBER) ([ghi]:FUNCTION [2]:NUMBER))", "abc(def(1), ghi(2))");
-        assertParsing("([position]:FUNCTION ['abc']:STRING [campo]:IDENTIFIER)", "position('abc' in campo)");
-        assertParsing("([CAST]:FUNCTION ([as]:ALIAS [campo2]:IDENTIFIER [VARCHAR]:IDENTIFIER))", "CAST(campo2 as VARCHAR)");
+        assertParsing("([position]:FUNCTION ['abc']:STRING [column]:IDENTIFIER)", "position('abc' in column)");
+        assertParsing("([CAST]:FUNCTION ([as]:ALIAS [column2]:IDENTIFIER [VARCHAR]:IDENTIFIER))", "CAST(column2 as VARCHAR)");
     }
 
     @Test
     public void selectFrom() {
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT [campo1]:IDENTIFIER [campo2]:IDENTIFIER) ([FROM]:FROM [tabela]:IDENTIFIER [tabela2]:IDENTIFIER))", "SELECT campo1, campo2 FROM tabela, tabela2");
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [tabela]:IDENTIFIER))", "SELECT * FROM tabela");
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [tabela]:IDENTIFIER ([INNER JOIN]:JOIN [outra_tabela]:IDENTIFIER ([ON]:ON [xxx]:IDENTIFIER))))", "SELECT * FROM tabela INNER JOIN outra_tabela ON xxx");
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT [column1]:IDENTIFIER [column2]:IDENTIFIER) ([FROM]:FROM [table]:IDENTIFIER [table2]:IDENTIFIER))", "SELECT column1, column2 FROM table, table2");
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [table]:IDENTIFIER))", "SELECT * FROM table");
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [table]:IDENTIFIER ([INNER JOIN]:JOIN [outra_table]:IDENTIFIER ([ON]:ON [xxx]:IDENTIFIER))))", "SELECT * FROM table INNER JOIN outra_table ON xxx");
 
     }
 
     @Test
     public void selectWithWhere(){
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [tabela]:IDENTIFIER) ([WHERE]:WHERE ([+]:ARITHMETIC [campo]:IDENTIFIER [2]:NUMBER)))", "SELECT  * FROM tabela WHERE campo + 2");
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [table]:IDENTIFIER) ([WHERE]:WHERE ([+]:ARITHMETIC [column]:IDENTIFIER [2]:NUMBER)))", "SELECT  * FROM table WHERE column + 2");
 
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [tabela]:IDENTIFIER) ([WHERE]:WHERE ([=]:OPERATOR [tabela.campo1]:IDENTIFIER [tabela.campo2]:IDENTIFIER)))", "SELECT  * FROM tabela WHERE tabela.campo1 = tabela.campo2");
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [table]:IDENTIFIER) ([WHERE]:WHERE ([=]:OPERATOR [table.column1]:IDENTIFIER [table.column2]:IDENTIFIER)))", "SELECT  * FROM table WHERE table.column1 = table.column2");
 
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [tabela]:IDENTIFIER) ([WHERE]:WHERE ([AND]:BOOLEAN ([=]:OPERATOR [campo]:IDENTIFIER [2]:NUMBER) ([=]:OPERATOR [id]:IDENTIFIER [3]:NUMBER))))",
-            "SELECT * FROM tabela WHERE campo = 2 AND id = 3");
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [table]:IDENTIFIER) ([WHERE]:WHERE ([AND]:BOOLEAN ([=]:OPERATOR [column]:IDENTIFIER [2]:NUMBER) ([=]:OPERATOR [id]:IDENTIFIER [3]:NUMBER))))",
+            "SELECT * FROM table WHERE column = 2 AND id = 3");
 
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [tabela]:IDENTIFIER) ([WHERE]:WHERE ([OR]:BOOLEAN ([AND]:BOOLEAN ([=]:OPERATOR [campo]:IDENTIFIER [2]:NUMBER) ([=]:OPERATOR [id]:IDENTIFIER [:param]:PARAMETER)) ([=]:OPERATOR [campo]:IDENTIFIER [5.5]:NUMBER))))",
-            "SELECT * FROM tabela WHERE campo = 2 AND id = :param OR campo = 5.5");
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [table]:IDENTIFIER) ([WHERE]:WHERE ([OR]:BOOLEAN ([AND]:BOOLEAN ([=]:OPERATOR [column]:IDENTIFIER [2]:NUMBER) ([=]:OPERATOR [id]:IDENTIFIER [:param]:PARAMETER)) ([=]:OPERATOR [column]:IDENTIFIER [5.5]:NUMBER))))",
+            "SELECT * FROM table WHERE column = 2 AND id = :param OR column = 5.5");
 
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [tabela]:IDENTIFIER) ([WHERE]:WHERE ([OR]:BOOLEAN ([AND]:BOOLEAN ([(]:PARENTHESIS ([=]:OPERATOR [campo]:IDENTIFIER [2]:NUMBER)) ([=]:OPERATOR [id]:IDENTIFIER [35.89]:NUMBER)) ([(]:PARENTHESIS ([=]:OPERATOR [campo]:IDENTIFIER [5]:NUMBER)))))",
-            "SELECT * FROM tabela WHERE (campo = 2) AND id = 35.89 OR (campo = 5)");
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [tabela]:IDENTIFIER) ([WHERE]:WHERE ([>]:OPERATOR [campo]:IDENTIFIER [2]:NUMBER)))", "SELECT  * FROM tabela WHERE campo >2");
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [table]:IDENTIFIER) ([WHERE]:WHERE ([OR]:BOOLEAN ([AND]:BOOLEAN ([(]:PARENTHESIS ([=]:OPERATOR [column]:IDENTIFIER [2]:NUMBER)) ([=]:OPERATOR [id]:IDENTIFIER [35.89]:NUMBER)) ([(]:PARENTHESIS ([=]:OPERATOR [column]:IDENTIFIER [5]:NUMBER)))))",
+            "SELECT * FROM table WHERE (column = 2) AND id = 35.89 OR (column = 5)");
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [table]:IDENTIFIER) ([WHERE]:WHERE ([>]:OPERATOR [column]:IDENTIFIER [2]:NUMBER)))", "SELECT  * FROM table WHERE column >2");
     }
 
     @Test
     public void selectWithAlias(){
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT [tb.campo1]:IDENTIFIER) ([FROM]:FROM [tabela]:IDENTIFIER))", "SELECT tb.campo1 FROM tabela");
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT ([AS]:ALIAS [campo]:IDENTIFIER [nome]:IDENTIFIER)) ([FROM]:FROM [tabela]:IDENTIFIER))", "SELECT campo AS nome FROM tabela");
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT ([AS]:ALIAS [tb.campo1]:IDENTIFIER [nome]:IDENTIFIER)) ([FROM]:FROM [tabela]:IDENTIFIER))", "SELECT tb.campo1 AS nome FROM tabela");
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT [tb.column1]:IDENTIFIER) ([FROM]:FROM [table]:IDENTIFIER))", "SELECT tb.column1 FROM table");
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT ([AS]:ALIAS [column]:IDENTIFIER [nome]:IDENTIFIER)) ([FROM]:FROM [table]:IDENTIFIER))", "SELECT column AS nome FROM table");
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT ([AS]:ALIAS [tb.column1]:IDENTIFIER [nome]:IDENTIFIER)) ([FROM]:FROM [table]:IDENTIFIER))", "SELECT tb.column1 AS nome FROM table");
     }
 
     @Test
     public void selectWithConcat(){
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT ([||]:CONCAT [campo1]:IDENTIFIER [campo2]:IDENTIFIER)) ([FROM]:FROM [tabela]:IDENTIFIER))", "SELECT campo1 || campo2 FROM tabela");
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT ([||]:CONCAT [campo1]:IDENTIFIER [campo2]:IDENTIFIER [campo3]:IDENTIFIER)) ([FROM]:FROM [tabela]:IDENTIFIER))",
-                "SELECT campo1 || campo2 || campo3 FROM tabela");
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT ([||]:CONCAT [campo1]:IDENTIFIER [campo2]:IDENTIFIER ([abc]:FUNCTION [campo3]:IDENTIFIER [campo4]:IDENTIFIER))) ([FROM]:FROM [tabela]:IDENTIFIER))",
-                "SELECT campo1 || campo2 || abc(campo3, campo4) FROM tabela");
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT ([||]:CONCAT [campo1]:IDENTIFIER ['string']:STRING ([abc]:FUNCTION [campo3]:IDENTIFIER [campo4]:IDENTIFIER))) ([FROM]:FROM [tabela]:IDENTIFIER))",
-                "SELECT campo1 || 'string' || abc(campo3, campo4) FROM tabela");
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT ([||]:CONCAT [column1]:IDENTIFIER [column2]:IDENTIFIER)) ([FROM]:FROM [table]:IDENTIFIER))", "SELECT column1 || column2 FROM table");
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT ([||]:CONCAT [column1]:IDENTIFIER [column2]:IDENTIFIER [column3]:IDENTIFIER)) ([FROM]:FROM [table]:IDENTIFIER))",
+                "SELECT column1 || column2 || column3 FROM table");
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT ([||]:CONCAT [column1]:IDENTIFIER [column2]:IDENTIFIER ([abc]:FUNCTION [column3]:IDENTIFIER [column4]:IDENTIFIER))) ([FROM]:FROM [table]:IDENTIFIER))",
+                "SELECT column1 || column2 || abc(column3, column4) FROM table");
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT ([||]:CONCAT [column1]:IDENTIFIER ['string']:STRING ([abc]:FUNCTION [column3]:IDENTIFIER [column4]:IDENTIFIER))) ([FROM]:FROM [table]:IDENTIFIER))",
+                "SELECT column1 || 'string' || abc(column3, column4) FROM table");
     }
 
     @Test
@@ -141,64 +141,64 @@ public class ParserTest {
 
      @Test
     public void selectOrder(){
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [tabela]:IDENTIFIER) ([WHERE]:WHERE ([=]:OPERATOR [campo]:IDENTIFIER [2]:NUMBER)) ([ORDER BY]:ORDER [campo2]:IDENTIFIER))", "SELECT  * FROM tabela WHERE campo = 2 ORDER BY campo2");
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [tabela]:IDENTIFIER) ([WHERE]:WHERE ([=]:OPERATOR [campo]:IDENTIFIER [2]:NUMBER)) ([ORDER BY]:ORDER [campo2]:IDENTIFIER [campo3]:IDENTIFIER [campo4]:IDENTIFIER))", "SELECT  * FROM tabela WHERE campo = 2 ORDER BY campo2, campo3, campo4");
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [tabela]:IDENTIFIER) ([ORDER BY]:ORDER [campo2]:IDENTIFIER ([DESC]:ORDERING [campo3]:IDENTIFIER) ([ASC]:ORDERING [campo4]:IDENTIFIER)))", "SELECT  * FROM tabela ORDER BY campo2, campo3 DESC, campo4 ASC");
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [table]:IDENTIFIER) ([WHERE]:WHERE ([=]:OPERATOR [column]:IDENTIFIER [2]:NUMBER)) ([ORDER BY]:ORDER [column2]:IDENTIFIER))", "SELECT  * FROM table WHERE column = 2 ORDER BY column2");
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [table]:IDENTIFIER) ([WHERE]:WHERE ([=]:OPERATOR [column]:IDENTIFIER [2]:NUMBER)) ([ORDER BY]:ORDER [column2]:IDENTIFIER [column3]:IDENTIFIER [column4]:IDENTIFIER))", "SELECT  * FROM table WHERE column = 2 ORDER BY column2, column3, column4");
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [table]:IDENTIFIER) ([ORDER BY]:ORDER [column2]:IDENTIFIER ([DESC]:ORDERING [column3]:IDENTIFIER) ([ASC]:ORDERING [column4]:IDENTIFIER)))", "SELECT  * FROM table ORDER BY column2, column3 DESC, column4 ASC");
     }
 
     @Test
     public void selectLimit(){
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [tabela]:IDENTIFIER) ([WHERE]:WHERE ([=]:OPERATOR [campo]:IDENTIFIER [2]:NUMBER)) ([LIMIT]:LIMIT [10]:NUMBER))", "SELECT  * FROM tabela WHERE campo = 2  LIMIT 10");
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [tabela]:IDENTIFIER) ([WHERE]:WHERE ([=]:OPERATOR [campo]:IDENTIFIER [2]:NUMBER)) ([ORDER BY]:ORDER [campo2]:IDENTIFIER) ([LIMIT]:LIMIT [10]:NUMBER ([OFFSET]:OFFSET [0]:NUMBER)))", "SELECT  * FROM tabela WHERE campo = 2 ORDER BY campo2 LIMIT 10 OFFSET 0");
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [table]:IDENTIFIER) ([WHERE]:WHERE ([=]:OPERATOR [column]:IDENTIFIER [2]:NUMBER)) ([LIMIT]:LIMIT [10]:NUMBER))", "SELECT  * FROM table WHERE column = 2  LIMIT 10");
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [table]:IDENTIFIER) ([WHERE]:WHERE ([=]:OPERATOR [column]:IDENTIFIER [2]:NUMBER)) ([ORDER BY]:ORDER [column2]:IDENTIFIER) ([LIMIT]:LIMIT [10]:NUMBER ([OFFSET]:OFFSET [0]:NUMBER)))", "SELECT  * FROM table WHERE column = 2 ORDER BY column2 LIMIT 10 OFFSET 0");
     }
 
      @Test
     public void TestCase(){
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT ([CASE]:CASE [campo]:IDENTIFIER ([WHEN]:WHEN [1]:NUMBER ([THEN]:THEN ['Aguarda DistribuiÃ§Ã£o']:STRING)) ([WHEN]:WHEN [2]:NUMBER ([THEN]:THEN ['Em AnÃ¡lise']:STRING)) ([ELSE]:ELSE ['']:STRING) [END]:END)) ([FROM]:FROM ([as]:ALIAS [sat00100]:IDENTIFIER [sa001]:IDENTIFIER)))",
-              " SELECT  CASE campo"
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT ([CASE]:CASE [column]:IDENTIFIER ([WHEN]:WHEN [1]:NUMBER ([THEN]:THEN ['Aguarda DistribuiÃ§Ã£o']:STRING)) ([WHEN]:WHEN [2]:NUMBER ([THEN]:THEN ['Em AnÃ¡lise']:STRING)) ([ELSE]:ELSE ['']:STRING) [END]:END)) ([FROM]:FROM ([as]:ALIAS [sat00100]:IDENTIFIER [sa001]:IDENTIFIER)))",
+              " SELECT  CASE column"
             + " WHEN 1  THEN  'Aguarda DistribuiÃ§Ã£o'"
             + " WHEN 2  THEN  'Em AnÃ¡lise'"
             + " ELSE ''  END "
             + " FROM sat00100 as sa001");
 
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT ([CASE]:CASE ([*]:ARITHMETIC [x]:IDENTIFIER [5]:NUMBER) ([WHEN]:WHEN [1]:NUMBER ([THEN]:THEN ([=]:OPERATOR [msg]:IDENTIFIER ['one or two']:STRING))) ([ELSE]:ELSE ([=]:OPERATOR [msg]:IDENTIFIER ['other value than one or two']:STRING)) [END]:END)) ([FROM]:FROM [TABELA]:IDENTIFIER))",
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT ([CASE]:CASE ([*]:ARITHMETIC [x]:IDENTIFIER [5]:NUMBER) ([WHEN]:WHEN [1]:NUMBER ([THEN]:THEN ([=]:OPERATOR [msg]:IDENTIFIER ['one or two']:STRING))) ([ELSE]:ELSE ([=]:OPERATOR [msg]:IDENTIFIER ['other value than one or two']:STRING)) [END]:END)) ([FROM]:FROM [table]:IDENTIFIER))",
                 "SELECT CASE x*5 "
                 + "WHEN 1 THEN msg = 'one or two' "
                 + "ELSE msg = 'other value than one or two'"
                 + "END "
-                + "FROM TABELA");
+                + "FROM table");
 
     }
 
      @Test
     public void subSelect(){
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT [campo]:IDENTIFIER) ([FROM]:FROM ([(]:PARENTHESIS ([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [tabela]:IDENTIFIER)))))",
-                "SELECT campo FROM (SELECT * FROM tabela)");
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT [campo]:IDENTIFIER) ([FROM]:FROM [tabela]:IDENTIFIER) ([WHERE]:WHERE ([EXISTS]:FUNCTION ([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [tabela]:IDENTIFIER)))))",
-                "SELECT campo FROM tabela WHERE EXISTS(SELECT * FROM tabela)");
-        assertParsing("([SQL]:SQL ([SELECT]:SELECT ([CASE]:CASE [campo]:IDENTIFIER ([WHEN]:WHEN ([EXISTS]:FUNCTION ([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [tabela]:IDENTIFIER))) ([THEN]:THEN ['ok']:STRING)) [END]:END)) ([FROM]:FROM [tabela]:IDENTIFIER))",
-                "SELECT CASE campo WHEN EXISTS(SELECT * FROM tabela) THEN 'ok' END FROM tabela");
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT [column]:IDENTIFIER) ([FROM]:FROM ([(]:PARENTHESIS ([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [table]:IDENTIFIER)))))",
+                "SELECT column FROM (SELECT * FROM table)");
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT [column]:IDENTIFIER) ([FROM]:FROM [table]:IDENTIFIER) ([WHERE]:WHERE ([EXISTS]:FUNCTION ([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [table]:IDENTIFIER)))))",
+                "SELECT column FROM table WHERE EXISTS(SELECT * FROM table)");
+        assertParsing("([SQL]:SQL ([SELECT]:SELECT ([CASE]:CASE [column]:IDENTIFIER ([WHEN]:WHEN ([EXISTS]:FUNCTION ([SQL]:SQL ([SELECT]:SELECT [*]:IDENTIFIER) ([FROM]:FROM [table]:IDENTIFIER))) ([THEN]:THEN ['ok']:STRING)) [END]:END)) ([FROM]:FROM [table]:IDENTIFIER))",
+                "SELECT CASE column WHEN EXISTS(SELECT * FROM table) THEN 'ok' END FROM table");
 
 
     }
 
      @Test
      public void SelectGroup(){
-          assertParsing("([SQL]:SQL ([SELECT]:SELECT [DISTINCT]:DISTINCT [ax050.idinterno]:IDENTIFIER [ax050.descricao]:IDENTIFIER ([||]:CONCAT ([=]:OPERATOR ['sistema']:STRING ([RTRIM]:FUNCTION [ax050.idinterno]:IDENTIFIER)) [' - ']:STRING ([RTRIM]:FUNCTION [ax050.descricao]:IDENTIFIER))) ([FROM]:FROM ([AS]:ALIAS [AXT05000]:IDENTIFIER [ax050]:IDENTIFIER)) ([WHERE]:WHERE ([like]:LIKE [ax050.Descricao]:IDENTIFIER ['SERVIÇOS DE TI%']:STRING)) ([GROUP BY]:GROUP [campo1]:IDENTIFIER [campo2]:IDENTIFIER))",
+          assertParsing("([SQL]:SQL ([SELECT]:SELECT [DISTINCT]:DISTINCT [ax050.idinterno]:IDENTIFIER [ax050.descricao]:IDENTIFIER ([||]:CONCAT ([=]:OPERATOR ['sistema']:STRING ([RTRIM]:FUNCTION [ax050.idinterno]:IDENTIFIER)) [' - ']:STRING ([RTRIM]:FUNCTION [ax050.descricao]:IDENTIFIER))) ([FROM]:FROM ([AS]:ALIAS [AXT05000]:IDENTIFIER [ax050]:IDENTIFIER)) ([WHERE]:WHERE ([like]:LIKE [ax050.Descricao]:IDENTIFIER ['SERVIÇOS DE TI%']:STRING)) ([GROUP BY]:GROUP [column1]:IDENTIFIER [column2]:IDENTIFIER))",
                 "SELECT DISTINCT ax050.idinterno, ax050.descricao,    "
             + "                        'sistema' = RTRIM(ax050.idinterno) || ' - ' || RTRIM(ax050.descricao)    "
             + "           FROM AXT05000 AS ax050    "
             + "            WHERE ax050.Descricao like 'SERVIÇOS DE TI%'  "
-            + " GROUP BY campo1, campo2");
+            + " GROUP BY column1, column2");
      }
 
      @Test
      public void notIs(){
          assertParsing("([NOT]:NOT [1]:NUMBER)", "NOT 1");
          assertParsing("([NOT]:NOT ([abc]:FUNCTION [1]:NUMBER))", "NOT abc(1)");
-         assertParsing("([NOT  LIKE]:LIKE [campo]:IDENTIFIER ['teste']:STRING)", "campo NOT  LIKE 'teste'");
-         assertParsing("([is]:IS [campo]:IDENTIFIER [null]:IDENTIFIER)", " campo is null");
-         assertParsing("([IS]:IS [campo]:IDENTIFIER ([NOT]:NOT [NULL]:IDENTIFIER))", "campo IS NOT NULL");
+         assertParsing("([NOT  LIKE]:LIKE [column]:IDENTIFIER ['teste']:STRING)", "column NOT  LIKE 'teste'");
+         assertParsing("([is]:IS [column]:IDENTIFIER [null]:IDENTIFIER)", " column is null");
+         assertParsing("([IS]:IS [column]:IDENTIFIER ([NOT]:NOT [NULL]:IDENTIFIER))", "column IS NOT NULL");
      }
 
     @Test
@@ -268,20 +268,20 @@ public class ParserTest {
             public Expression parse() {
                 Expression result = new Expression("SQL", "SQL");
 
-                Expression fields = new Expression("SELECT", "SELECT");
+                Expression columns = new Expression("SELECT", "SELECT");
 
                 if(canConsume(word("DISTINCT"))){
-                    fields.addChildren(new Expression("DISTINCT", lastMatch()));
+                    columns.addChildren(new Expression("DISTINCT", lastMatch()));
                 }
 
                 do {
-                    Expression field = nextExpression();
-                    if (field.isType("OPERATOR")) {
-                        Expression substitute = new Expression("ALIAS", field.printValue());
-                        substitute.addChildren(field.getChildren());
-                        field = substitute;
+                    Expression column = nextExpression();
+                    if (column.isType("OPERATOR")) {
+                        Expression substitute = new Expression("ALIAS", column.printValue());
+                        substitute.addChildren(column.getChildren());
+                        column = substitute;
                     }
-                    fields.addChildren(field);
+                    columns.addChildren(column);
                 } while (canConsume(","));
 
                 consumeIf(word("FROM"));
@@ -300,7 +300,7 @@ public class ParserTest {
                     join.addChildren(on);
                     from.addChildren(join);
                 }
-                result.addChildren(fields, from);
+                result.addChildren(columns, from);
 
                 if(canConsume(word("WHERE"))){
                     Expression where = new Expression("WHERE", lastMatch());
